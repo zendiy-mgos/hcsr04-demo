8edc0cb5f1d6ab16755dac08ac6549f738d63d7f
Filippo Balestrini <filippo.balestrini@it.abb.com>
2020-01-23 19:10:53 +0100

    Compile error fix

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "mgos.h"
 #include "mgos_timers.h"
+#include "mgos_sys_config.h"
 #include "mgos_hcsr04.h"
 
 static void my_timer_cb(void *arg) {
@@ -14,12 +15,12 @@ static void my_timer_cb(void *arg) {
 enum mgos_app_init_result mgos_app_init(void) {
   /* Create HC-SR04 sensor instance */
   struct mgos_hcsr04 *hcsr04 = mgos_hcsr04_create(
-    mgos_sys_config_app_hcsr04_trigger_pin_get(),
-    mgos_sys_config_app_hcsr04_echo_pin_get());
+    mgos_sys_config_get_app_hcsr04_trigger_pin(),
+    mgos_sys_config_get_app_hcsr04_echo_pin());
   if (hcsr04 == NULL) return MGOS_APP_INIT_ERROR;  
   
   /* Set timer for reading distance */
-  mgos_set_timer(mgos_sys_config_app_measure_interval_get(),
+  mgos_set_timer(mgos_sys_config_get_app_measure_interval(),
     MGOS_TIMER_REPEAT, my_timer_cb, hcsr04);
 
   return MGOS_APP_INIT_SUCCESS;
